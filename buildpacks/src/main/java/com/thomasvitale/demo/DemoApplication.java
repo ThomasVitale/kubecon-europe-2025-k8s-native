344123209fd385a3342e3ff7086875114431ed0b
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-03-24 22:42:58 +0100

    Add Buildpacks example

---
+++ buildpacks/src/main/java/com/thomasvitale/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.thomasvitale.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> router() {
+		return RouterFunctions.route()
+			.GET("/", _ -> ServerResponse.ok().body("It's London, baby!"))
+			.build();
+	}
+
+}
